92b574dfc4b07af36ee08ce33f65ae218c4a5968
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2022-07-10 15:27:04 +0200

    Add a custom endpoint

--- infrastructure/blog/storage.tf
+++ infrastructure/blog/storage.tf
@@ -18,21 +18,27 @@ resource "azurerm_storage_account" "blog" {
 }
 
 resource "azurerm_cdn_profile" "blog_cdn" {
-  name = "blogCdn"
-  location = var.az_location
+  name                = "blogCdn"
+  location            = var.az_location
   resource_group_name = var.az_resource_group
-  sku = "Standard_Microsoft"
+  sku                 = "Standard_Microsoft"
 }
 
 resource "azurerm_cdn_endpoint" "blog_endpoint" {
-  name = "CDN Endpoint"
-  profile_name = azurerm_cdn_profile.blog_cdn.name
-  location = var.az_location
+  name                = "CDN Endpoint"
+  profile_name        = azurerm_cdn_profile.blog_cdn.name
+  location            = var.az_location
   resource_group_name = var.az_resource_group
   origin {
-    name = "blog"
+    name      = "blog"
     host_name = azurerm_storage_account.blog.primary_blob_host
   }
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "blog_custom_endpoint" {
+  name            = "CDN Custom endpoint"
+  cdn_endpoint_id = azurerm_cdn_endpoint.blog_endpoint.id
+  host_name       = var.root_domain
 }
 
 output "CDN_Endpoint" {
