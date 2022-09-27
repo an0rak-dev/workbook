4e8c2a7a27258a493ca68ae37176c57a4b79a650
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2022-09-27 21:31:51 +0200

    Remove terraform : KISS

--- infrastructure/ReadMe.md
+++
@@ -1,7 +0,0 @@
-# Blog -- Infrastructure
-
-This folder contains, in Infrastructure as Code, all the infra 
-elements deployed for this project.
-
-* [codebase](./codebase/github/ReadMe.md) : The infrastructure to deploy 
-    _this_ particular repository.
--- infrastructure/blog/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "az_resource_group" {
-  type        = string
-  description = "Resource group containing the Azure infrastructure of the blog"
-}
-
-variable "az_location" {
-  type        = string
-  description = "Location of the Azure infrastructure elements"
-}
